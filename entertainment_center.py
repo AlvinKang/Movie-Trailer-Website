3d557f3941419fc8da3c53dfeafa962d0031f302
Alvin Kang <oneinallin1@gmail.com>
2017-08-07 11:52:14 -0700

    Instantiated inception and zootopia

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,9 +1,15 @@
 import media
 
 # Create 6 Movie objects
-inception_movie = media.Movie()
+inception_movie = media.Movie("Inception",
+							  "A man and his team goes into others' dreams",
+							  "http://cdn.collider.com/wp-content/uploads/Inception-movie-poster-4.jpg",
+							  "https://www.youtube.com/watch?v=YoHD9XEInc0")
 
-zootopia_movie = media.Movie()
+zootopia_movie = media.Movie("Zootopia",
+							 "A bunny and fox team up to solve the myster of Zootopia",
+							 "https://s-media-cache-ak0.pinimg.com/originals/5a/29/d7/5a29d758e586fa4c44ef0e9065b4651c.jpg",
+							 "https://www.youtube.com/watch?v=CzvH6_e2a-U")
 
 forrest_gump_movie = media.Movie()
 
