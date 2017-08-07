3ae4b55443647c4cb083263116724f97fa4bfb89
Alvin Kang <oneinallin1@gmail.com>
2017-08-07 11:49:19 -0700

    Add six movies empty constructor

---
+++ entertainment_center.py
@@ -0,0 +1,14 @@
+import media
+
+# Create 6 Movie objects
+inception_movie = media.Movie()
+
+zootopia_movie = media.Movie()
+
+forrest_gump_movie = media.Movie()
+
+walle_movie = media.Movie()
+
+lotr_movie = media.Movie()
+
+poc_movie = media.Movie()
