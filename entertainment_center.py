52030ea137f93b74460edf55f6d1b886f0d0d164
Alvin Kang <oneinallin1@gmail.com>
2017-08-07 12:01:59 -0700

    Add Forrest Gump and Wall-E movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,4 +1,5 @@
 import media
+import fresh_tomatoes
 
 # Create 6 Movie objects
 inception_movie = media.Movie("Inception",
@@ -11,10 +12,19 @@ zootopia_movie = media.Movie("Zootopia",
 							 "https://s-media-cache-ak0.pinimg.com/originals/5a/29/d7/5a29d758e586fa4c44ef0e9065b4651c.jpg",
 							 "https://www.youtube.com/watch?v=CzvH6_e2a-U")
 
-forrest_gump_movie = media.Movie()
+forrest_gump_movie = media.Movie("Forrest Gump",
+								 "Autistic man lives through numerous historical events",
+								 "http://www.impawards.com/1994/posters/forrest_gump_xlg.jpg",
+								 "https://www.youtube.com/watch?v=bLvqoHBptjg")
 
-walle_movie = media.Movie()
+walle_movie = media.Movie("Wall-E",
+						  "A robot couple brings humanity back to Earth",
+						  "https://img.yescdn.ru/2015/09/15/poster/WALL-E_1.jpg",
+						  "https://www.youtube.com/watch?v=ZAWIIlXNGwY")
 
-lotr_movie = media.Movie()
+# lotr_movie = media.Movie()
 
-poc_movie = media.Movie()
+# poc_movie = media.Movie()
+
+movies = [inception_movie, zootopia_movie, forrest_gump_movie, walle_movie]
+fresh_tomatoes.open_movies_page(movies)
