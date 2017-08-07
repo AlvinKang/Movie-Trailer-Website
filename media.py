95f6bd076c13b37bc4465298259ddf561acc5ad3
Alvin Kang <oneinallin1@gmail.com>
2017-08-07 11:42:14 -0700

    Add constructor comment

--- media.py
+++ media.py
@@ -1,6 +1,8 @@
 class Movie():
+	# Constructor
 	def __init__(self, movie_title, movie_summary, movie_poster_url, movie_trailer_url):
 		self.movie_title = movie_title
 		self.movie_summary = movie_summary
 		self.movie_poster_url = movie_poster_url
 		self.movie_trailer_url = movie_trailer_url
+
